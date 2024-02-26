c37b7341d0e08a29ea33fae55cffc6d77e2f50a7
dgodoy1996 <danielagdytcr@yahoo.com>
2024-02-26 13:24:43 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// constants (An object with cards and another object with copies of cards)
+let cards
+let cardsCopy
+
+// state variables (array of 6 columns, winner/not winner, )
+let board
+let outcome
+
+// cache elements (win/lose msg, timer, play again button)
+
+
+// event listeners (click on cards, timer, click play again)
+
+// functions (init, render, compare cards)
+renderFirstCard() {
+
+}
+
+renderSecondCard() {
+
+}
+renderRevealImage() {
+
+}
+frenderControls() {
+
+}
+
+renderMessages() {
+
+}
+
+function render() {
+    renderFirstCard();
+    renderSecondCard();
+    renderRevealImage();
+    renderControls();
+    renderMessages();
+  }
